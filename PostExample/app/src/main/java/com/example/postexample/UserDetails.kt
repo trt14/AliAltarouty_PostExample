c52c94941954c64be00a2abd54d9520ec3553e96
trt14 <tarooti14dev@gmail.com>
2021-10-11 13:37:37 +0300

    add

---
+++ PostExample/app/src/main/java/com/example/postexample/UserDetails.kt
@@ -0,0 +1,23 @@
+package com.example.postexample
+
+import com.google.gson.annotations.SerializedName
+
+
+class Users {
+
+    var data: List<UserDetails>? = null
+
+    class UserDetails {
+
+        @SerializedName("name")
+        var name: String? = null
+
+        @SerializedName("location")
+        var location: String? = null
+
+        constructor(name: String?, location: String?) {
+            this.name = name
+            this.location = location
+        }
+    }
+}
